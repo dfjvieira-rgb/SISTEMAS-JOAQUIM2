f6afbc838927bceaf8ef01eee583d29cda5d6277
dfjvieira-rgb <dfjvieira@gmail.com>
2026-02-01 10:50:44 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,3 @@
-// firebase-config.js - BASE BLINDADA
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
@@ -12,8 +11,5 @@ const firebaseConfig = {
     appId: "1:620234160245:web:d0ef18660600087f9ba03c"
 };
 
-// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
-
-// Exporta o banco de dados para ser usado no seu script principal
 export const db = getDatabase(app);
